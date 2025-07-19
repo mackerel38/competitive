44558a3b6964d75ac2db97e1013a22cbec6825ba
poe <153431867+mackerel38@users.noreply.github.com>
2025-07-19 11:59:34 +0000

    　

--- atcoder/abc/a/main.cpp
+++ atcoder/abc/a/main.cpp
@@ -2,9 +2,31 @@
 
 int main() { IO();
     int T=1;
-    // cin >> T;
+    cin >> T;
     while (T--) solve();
 }
 
+int modpow(int x, int n, int m) {
+    int re = 1;
+    int k = x;
+    while (n) {
+        if (n & 1) re = (1LL * re * k) % m;
+        k = (1LL * k * k) % m;
+        n >>= 1;
+    }
+    return re;
+}
+
 void solve() {
+    int n = 10000;
+    rep1(i, n) {
+        vi mp;
+        rep(j, n) {
+            mp.pb(modpow(j, 2, i));
+        }
+        uniq(mp);
+        if(mp.size() == 2) {
+            cout << i << sp;
+        }
+    }
 }
