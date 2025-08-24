c8e637208a693afce1d3c534a346217b7c3e28a6
mackerel38 <mackerel38383838@gmail.com>
2025-08-25 00:52:02 +0900

    バグ修正

--- library/graph/cartesiantree.hpp
+++ library/graph/cartesiantree.hpp
@@ -5,12 +5,12 @@ using namespace std;
 
 template <class T>
 pair<graph<int, true, false>, int> cartesiantree(vector<T> & a) {
-    graph<int, true, false> g(n);
+    graph<int, true, false> g(a.size());
     vector<int> p(a.size(), -1);
     stack<int> s;
     for (int i=0; i<(int)a.size(); i++) {
         int pre = -1;
-        while (!s.empty() && a[i] < a[s.back()]) {
+        while (!s.empty() && a[i] < a[s.top()]) {
             pre = s.top(); s.pop();
         }
         if (pre != -1) p[pre] = i;
