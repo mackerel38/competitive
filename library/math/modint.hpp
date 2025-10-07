8f63b126741f5cb2b854389d900c96914762fbcd
mackerel38 <mackerel38383838@gmail.com>
2025-10-07 20:40:30 +0900

    オーバーフローの回避, 整数型との演算を追加

--- library/math/modint.hpp
+++ library/math/modint.hpp
@@ -1,16 +1,25 @@
 #pragma once
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 template <long long modint_MOD>
 struct modint {
     long long val;
-    constexpr modint(long long x = 0) noexcept {
+    constexpr modint(long long x=0) noexcept {
         x %= modint_MOD;
         if (x < 0) x += modint_MOD;
         val = x;
     }
     explicit constexpr operator long long() const noexcept { return val; }
+    template<typename Int>
+    static constexpr long long adjust(Int x) noexcept {
+        static_assert(std::is_integral<Int>::value, "Int must be integral");
+        __int128 t = static_cast<__int128>(x);
+        __int128 m = static_cast<__int128>(modint_MOD);
+        t %= m;
+        if (t < 0) t += m;
+        return static_cast<long long>(t);
+    }
     constexpr modint operator+(const modint& x) const noexcept { return modint(*this) += x; }
     constexpr modint operator-(const modint& x) const noexcept { return modint(*this) -= x; }
     constexpr modint operator*(const modint& x) const noexcept { return modint(*this) *= x; }
@@ -24,23 +33,76 @@ struct modint {
         return *this;
     }
     constexpr modint& operator*=(const modint& x) noexcept {
-        val = val * x.val % modint_MOD;
+        __int128 t = (__int128)val * x.val;
+        val = (long long)(t % modint_MOD);
+        if (val < 0) val += modint_MOD;
         return *this;
     }
-    constexpr modint& operator/=(modint& x) noexcept {
+    constexpr modint& operator/=(modint x) noexcept {
         *this *= x.inv();
         return *this;
     }
     constexpr modint operator-() const noexcept { return modint() - *this; }
-    constexpr bool operator==(const modint& x) const noexcept { return val == x.val; }
-    constexpr bool operator!=(const modint& x) const noexcept { return val != x.val; }
     constexpr modint& operator++() noexcept { return *this += 1; }
     constexpr modint& operator--() noexcept { return *this -= 1; }
     constexpr modint operator++(int) noexcept { modint tmp = *this; ++*this; return tmp; }
     constexpr modint operator--(int) noexcept { modint tmp = *this; --*this; return tmp; }
+    constexpr bool operator==(const modint& x) const noexcept { return val == x.val; }
+    constexpr bool operator!=(const modint& x) const noexcept { return val != x.val; }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    constexpr modint& operator+=(Int x) noexcept {
+        long long y = adjust<Int>(x);
+        if (modint_MOD <= (val += y)) val -= modint_MOD;
+        return *this;
+    }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    constexpr modint& operator-=(Int x) noexcept {
+        long long y = adjust<Int>(x);
+        if ((val -= y) < 0) val += modint_MOD;
+        return *this;
+    }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    constexpr modint& operator*=(Int x) noexcept {
+        long long y = adjust<Int>(x);
+        __int128 t = (__int128)val * y;
+        val = (long long)(t % modint_MOD);
+        if (val < 0) val += modint_MOD;
+        return *this;
+    }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    constexpr modint& operator/=(Int x) noexcept {
+        modint t = modint(adjust<Int>(x));
+        *this /= t;
+        return *this;
+    }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    friend constexpr modint operator+(const modint& a, Int b) noexcept { modint t = a; t += b; return t; }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    friend constexpr modint operator+(Int a, const modint& b) noexcept { modint t = b; t += a; return t; }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    friend constexpr modint operator-(const modint& a, Int b) noexcept { modint t = a; t -= b; return t; }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    friend constexpr modint operator-(Int a, const modint& b) noexcept { modint t = modint(adjust<Int>(a)); t -= b; return t; }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    friend constexpr modint operator*(const modint& a, Int b) noexcept { modint t = a; t *= b; return t; }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    friend constexpr modint operator*(Int a, const modint& b) noexcept { modint t = b; t *= a; return t; }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    friend constexpr modint operator/(const modint& a, Int b) noexcept { modint t = a; t /= b; return t; }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    friend constexpr modint operator/(Int a, const modint& b) noexcept { modint t = modint(adjust<Int>(a)); t /= b; return t; }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    friend constexpr bool operator==(const modint& a, Int b) noexcept { return a.val == adjust<Int>(b); }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    friend constexpr bool operator==(Int a, const modint& b) noexcept { return adjust<Int>(a) == b.val; }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    friend constexpr bool operator!=(const modint& a, Int b) noexcept { return !(a == b); }
+    template<typename Int, typename = std::enable_if_t<std::is_integral<Int>::value>>
+    friend constexpr bool operator!=(Int a, const modint& b) noexcept { return !(a == b); }
     friend constexpr istream& operator>> (istream& i, modint& x) { long long y; i >> y; x = modint(y); return i; }
     friend constexpr ostream& operator<< (ostream& o, const modint& x) { o << x.val; return o; }
     constexpr modint pow(long long n) const noexcept {
+        if (n < 0) return inv().pow(-n);
         modint x = *this, re = 1;
         while (0 < n){
             if (n & 1) re *= x;
