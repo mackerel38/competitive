0135585aa26b1c93a158bd5bd152131105315b72
mackerel38 <mackerel38383838@gmail.com>
2025-11-17 12:00:52 +0900

    erasablepq, 双方向pqの実装

---
+++ library/structure/bipq.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "erasablepq"
+#include <bits/stdc++.h>
+using namespace std;
+
+template <class T>
+struct bipq {
+    erasablepq<T> q;
+    erasablepqg<T> qg;
+    void push(const T& x) { q.push(x); qg.push(x); }
+    void erase(const T& x) { q.erase(x); qg.erase(x); }
+    void pop_max() { q.pop(); }
+    void pop_min() { qg.pop(); }
+    T top_max() { return q.top(); }
+    T top_min() { return qg.top(); }
+    bool empty() { return q.empty(); }
+};
---
+++ library/structure/erasablepq.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+template <class T>
+struct erasablepq {
+    priority_queue<T> pq, del;
+    void push(const T& x) { pq.push(x); }
+    void erase(const T& x) { del.push(x); }
+    void pop() {
+        normalize();
+        pq.pop();
+    }
+    T top() {
+        normalize();
+        return pq.top();
+    }
+    bool empty() {
+        normalize();
+        return pq.empty();
+    }
+    void normalize() {
+        while (!del.empty() && !pq.empty() && pq.top() == del.top()) {
+            pq.pop();
+            del.pop();
+        }
+    }
+};
+
+template <class T>
+struct erasablepqg {
+    priority_queue<T, vector<T>, greater<T>> pq, del;
+    void push(const T& x) { pq.push(x); }
+    void erase(const T& x) { del.push(x); }
+    void pop() {
+        normalize();
+        pq.pop();
+    }
+    T top() {
+        normalize();
+        return pq.top();
+    }
+    bool empty() {
+        normalize();
+        return pq.empty();
+    }
+    void normalize() {
+        while (!del.empty() && !pq.empty() && pq.top() == del.top()) {
+            pq.pop();
+            del.pop();
+        }
+    }
+};
